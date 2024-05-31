08bede9764645495ae1a11b048ac3f6fc62f113c
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2024-05-31 10:05:42 +0200

    Separate detection calls init

--- EyeTracker/Program.cs
+++ EyeTracker/Program.cs
@@ -54,30 +54,55 @@ namespace EyeTracker
             {
                 videoCapture.Read(frame);
 
-                
                 CvInvoke.CvtColor(frame, frameGray, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
-                CvInvoke.GaussianBlur(frameGray, frameBlurred, new Size(3, 3), 0);
+                CvInvoke.GaussianBlur(frameGray, frameBlurred, new Size(5, 5), 0);
                 CvInvoke.EqualizeHist(frameGray, frameEqualized);
 
-                var faces = faceCascade.DetectMultiScale(frameBlurred, 1.2, 10);
-                var leftEye = leftEyeCascade.DetectMultiScale(frameBlurred, 1.15, 8);
-                var rightEye = rightEyeCascade.DetectMultiScale(frameBlurred, 1.15, 8);
+                var faces = faceCascade.DetectMultiScale(frameEqualized, 1.2, 10);
+                var leftEye = leftEyeCascade.DetectMultiScale(frameEqualized, 1.15, 12);
+                var rightEye = rightEyeCascade.DetectMultiScale(frameEqualized, 1.15, 12);
 
                 if (faces != null && faces.Length > 0)
-                    CvInvoke.Rectangle(frame, faces[0], new MCvScalar(0, 255, 0), 2);
+                    CvInvoke.Rectangle(frameEqualized, faces[0], new MCvScalar(0, 255, 0), 2);
 
                 if (leftEye != null && leftEye.Length > 0)
-                    CvInvoke.Rectangle(frame, leftEye[0], new MCvScalar(255, 0, 0), 1);
+                    CvInvoke.Rectangle(frameEqualized, leftEye[0], new MCvScalar(255, 0, 0), 1);
 
                 if (rightEye != null && rightEye.Length > 0)
-                    CvInvoke.Rectangle(frame, rightEye[0], new MCvScalar(255, 0, 0), 1);
+                    CvInvoke.Rectangle(frameEqualized, rightEye[0], new MCvScalar(255, 0, 0), 1);
+
+                CvInvoke.Rectangle(frameEqualized, new Rectangle(new Point(10, 10), new Size(50, 50)), new MCvScalar(255, 0, 0), 1);
 
 
-                CvInvoke.Imshow("faceDetection", frame);
+                CvInvoke.Imshow("faceDetection", frameEqualized);
 
                 if (CvInvoke.WaitKey(1) == 27)
                     break;
             }
+        }
+
+        public static Rectangle[] DetectFace(Mat image)
+        {
+            var faceClassifier = new CascadeClassifier("./detection/haarcascade_frontalface_default.xml");
+            double scaleFactor = 1.2;
+            int minNeighbours = 10;
+            Size minSize = new Size(110, 110);
+            Size maxSize = new Size(275, 275);
+
+            var faceRectangles = faceClassifier.DetectMultiScale(image, scaleFactor, minNeighbours, minSize, maxSize);
+
+            return faceRectangles;
+        }
+
+        public static Rectangle[] DetectLeftEye(Mat image)
+        {
+            var eyeClassifier = new CascadeClassifier("./detection/haarcascade_lefteye_2splits.xml");
+            double scaleFactor = 1.15;
+            int minNeighbours = 12;
+
+            var leftEyeRectangles = eyeClassifier.DetectMultiScale(image, scaleFactor, minNeighbours);
+
+            return leftEyeRectangles;
         }
 
     }
