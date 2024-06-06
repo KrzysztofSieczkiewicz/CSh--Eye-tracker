0685f8129711af8b91cfd73f09935392e05bd54a
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2024-06-06 23:27:02 +0200

    init FaceDetection class

---
+++ EyeTracker/faceDetection/HaarFaceDetector.cs
@@ -0,0 +1,29 @@
+﻿using Emgu.CV;
+
+namespace EyeTracker.faceDetection
+{
+    internal class HaarFaceDetector : IFaceDetector
+    {
+        private CascadeClassifier faceClassifier;
+        private Mat image;
+
+        /* DEFAULT DETECTION SETTINGS */
+        private double scaleFactor = 1.2;
+        private int minNeighbours = 4;
+        private Size minSize = new Size(110, 110);
+        private Size maxSize = new Size(275, 275);
+
+
+        public HaarFaceDetector() 
+        {
+            faceClassifier = new CascadeClassifier("./detection/haarcascade_frontalface_default.xml");
+
+        }
+
+        public DetectedFace DetectFace()
+        {
+            return new DetectedFace();
+        }
+
+    }
+}
